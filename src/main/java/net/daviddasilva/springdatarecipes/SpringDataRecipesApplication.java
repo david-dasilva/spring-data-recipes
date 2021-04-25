5366a10fdbcc0bb72d3f80dd2e70074a945b3c01
David Da Silva <david.dasilva@zenika.com>
2021-04-25 12:00:36 +0200

    Initial commit

---
+++ src/main/java/net/daviddasilva/springdatarecipes/SpringDataRecipesApplication.java
@@ -0,0 +1,13 @@
+package net.daviddasilva.springdatarecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataRecipesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/daviddasilva/springdatarecipes/SpringDataRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.daviddasilva.springdatarecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
